be27610def4d77c9ce42d3b0ac64eb6790056fa1
0704681032 <307517019@qq.com>
2016-01-21 18:37:10 +0800

    ccc

--- getnotplayedpid.js
+++ getnotplayedpid.js
@@ -1,11 +1,16 @@
-//获取show页面没有播放的广告位
+//获取播放量为0的广告位 检查bug
 var trs = [].slice.call( document.querySelectorAll('td[title="Played"]') );
-var rs = trs.filter( i=>i.innerHTML==0).map(i=>{
+var played = trs.filter( i=> parseInt(i.innerHTML)||0 > 0).map(i=>{
 	return i.parentNode.parentNode.childNodes[1].childNodes[0].innerHTML;
 });
-rs = rs.reduce((obj,r)=>{
-	if ( !~obj.indexOf(r) ) {
+var notplayed = trs.filter( i=>i.innerHTML==0).map(i=>{
+	return i.parentNode.parentNode.childNodes[1].childNodes[0].innerHTML;
+});
+//去重
+rs = notplayed.reduce((obj,r)=>{
+	if ( !~obj.indexOf(r) && !~played.indexOf(r) ) {
 		obj.push(r);
 	}
 	return obj;
 },[]).sort();
+console.log(rs);
